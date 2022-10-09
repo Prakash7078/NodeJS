4e12df2c10cf7513a165e26fbb3bf3f69787ee05
Prakash7078 <20bq1a05j5@vvit.net>
2022-10-09 22:55:03 +0530

    use the post,get methods with express module

--- express1.js
+++ express1.js
@@ -1,8 +1,26 @@
 const express=require('express');
 const http=require('http');
+const morgan=require('morgan');
+const bodyParser=require('body-parser');
+
 const port=3000;
 const hostname='localhost';
 const app=express();
+app.use(morgan('dev'));
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.json());
+app.all('/dishes',(req,res,next)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+});
+
+app.get('/dishes:dishId',(req,res,next)=>{
+    res.end('will send details of dish to u'+req.params.dishId);
+});
+app.post('/dishes',(req,res,next)=>{
+    res.end('will add this dish'+req.body.name+'with details'+req.body.description);
+});
 app.use((req,res,next)=>
 {
     console.log(req.headers);
